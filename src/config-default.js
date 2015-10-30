83784a64a69ca760bb45907e9c521f141384c9d9
Rube <353371737@qq.com>
2015-10-30 13:54:41 +0800

    add config-p

---
+++ src/config-default.js
@@ -0,0 +1,22 @@
+/**
+ * @page
+ * @module
+ * @author Rube
+ * @date 15/10/29
+ * @desc
+ */
+module.exports = {
+    server: {
+        host: '127.0.0.1',
+        port: 8656
+    },
+    db: {
+        url: 'mongodb://127.0.0.1:27017/weixin-vote'
+    },
+    private: {
+        salt: 'weixin-vote'
+    },
+    rurl: 'http://www.doubleeggs.com:8656/v/w',
+    appid:'',
+    skey:''
+};
